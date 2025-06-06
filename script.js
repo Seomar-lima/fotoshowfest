fb2cc6671a42fa1973c3f1cd66d4dbd13cecf6a5
Seomar-lima <seomarlima2@gmail.com>
2025-06-06 17:49:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -90,3 +90,16 @@ fotoBtn.onclick = () => {
 bumerangueBtn.onclick = () => {
   alert("Gravação de bumerangue ainda em desenvolvimento.");
 };
+
+#qrDownload {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+}
+
+#qrDownload canvas {
+  background: white;
+  padding: 8px;
+  border-radius: 10px;
+}
